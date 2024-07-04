0db2c26a8363625eafb1d311fb4ed87ccb48ba42
DimaGAV <gav_gav82@mail.ru>
2024-07-04 19:51:48 +0300

    +controls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Burger from "@/components/Burger/Burger";
+import Controls from "@/components/Controls/Controls";
 import Filter from "@/components/Filter/Filter";
 import Menu from "@/components/Menu/Menu";
 import Sidebar from "@/components/Sidebar/Sidebar";
@@ -776,33 +777,7 @@ export default function Home() {
         <div className="bar__player-progress" />
         <div className="bar__player-block">
           <div className="bar__player player">
-            <div className="player__controls">
-              <div className="player__btn-prev">
-                <svg className="player__btn-prev-svg">
-                  <use xlinkHref="img/icon/sprite.svg#icon-prev" />
-                </svg>
-              </div>
-              <div className="player__btn-play _btn">
-                <svg className="player__btn-play-svg">
-                  <use xlinkHref="img/icon/sprite.svg#icon-play" />
-                </svg>
-              </div>
-              <div className="player__btn-next">
-                <svg className="player__btn-next-svg">
-                  <use xlinkHref="img/icon/sprite.svg#icon-next" />
-                </svg>
-              </div>
-              <div className="player__btn-repeat _btn-icon">
-                <svg className="player__btn-repeat-svg">
-                  <use xlinkHref="img/icon/sprite.svg#icon-repeat" />
-                </svg>
-              </div>
-              <div className="player__btn-shuffle _btn-icon">
-                <svg className="player__btn-shuffle-svg">
-                  <use xlinkHref="img/icon/sprite.svg#icon-shuffle" />
-                </svg>
-              </div>
-            </div>
+            <Controls/>
             <div className="player__track-play track-play">
               <div className="track-play__contain">
                 <div className="track-play__image">
---
+++ src/components/Controls/Controls.tsx
@@ -0,0 +1,34 @@
+import styles from "./Controls.module.css"
+
+
+const Controls = () => {
+    return ( <div className={styles.playerControls}>
+        <div className={styles.playerBtnPrev}>
+          <svg className={styles.playerBtnPrevSvg}>
+            <use xlinkHref="img/icon/sprite.svg#icon-prev" />
+          </svg>
+        </div>
+        <div className={styles.playerBtnPlay}>
+          <svg className={styles.playerBtnPlaySvg}>
+            <use xlinkHref="img/icon/sprite.svg#icon-play" />
+          </svg>
+        </div>
+        <div className={styles.playerBtnNext}>
+          <svg className={styles.playerBtnNextSvg}>
+            <use xlinkHref="img/icon/sprite.svg#icon-next" />
+          </svg>
+        </div>
+        <div className={styles.playerBtnRepeat}>
+          <svg className={styles.playerBtnRepeatSvg}>
+            <use xlinkHref="img/icon/sprite.svg#icon-repeat" />
+          </svg>
+        </div>
+        <div className={styles.playerBtnShuffle}>
+        <svg className={styles.playerBtnShuffleSvg}>
+            <use xlinkHref="img/icon/sprite.svg#icon-shuffle" />
+          </svg>
+        </div>
+      </div> );
+}
+ 
+export default Controls;
