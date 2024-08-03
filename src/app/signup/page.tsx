587b99ad7d1d7e5f5cff4cb2e68be24fdf1b78f7
DimaGAV <gav82gav@gmail.com>
2024-08-03 17:01:18 +0300

    56

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { /* getTokensState, getUser,  */regUser } from "@/store/features/userSlice";
+import { regUser } from "@/store/features/userSlice";
 import { useAppDispatch } from "@/hooks";
 import Link from "next/link";
 import { ChangeEvent, useState } from "react";
@@ -19,11 +19,11 @@ function SignUpPage() {
   });
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target; // Извлекаем имя поля и его значение
+    const { name, value } = event.target;
 
     setFormData({
-      ...formData, // Копируем текущие данные из состояния
-      [name]: value, // Обновляем нужное поле
+      ...formData,
+      [name]: value,
     });
   };
 
@@ -40,25 +40,18 @@ function SignUpPage() {
       return alert("заполнить поля!");
     }
     try {
-      // Диспетчеризация thunk getUser и разворачивание результата с помощью unwrap
       await dispatch(regUser(userData)).unwrap();
-      // Действия после успешного выполнения thunk, если необходимо
       console.log("Успешно!");
-
-      //функция получения токенов
-    //   await dispatch(getTokensState(userData)).unwrap();
-
-      router.push("/signin");
+      router.push("/");
     } catch (error: unknown) {
       if (error instanceof Error) {
-        // Обработка ошибки, если thunk завершился неудачно
         console.error("Ошибка:", error.message);
       }
     }
   };
 
   return (
-  <div className={styles.wrapper}>
+    <div className={styles.wrapper}>
       <div className={styles.containerEnter}>
         <div className={styles.modalBlock}>
           <form action="#" className={styles.modalFormLogin}>
@@ -88,7 +81,7 @@ function SignUpPage() {
               placeholder="Пароль"
               type="password"
             />
-                        <input
+            <input
               onChange={handleInputChange}
               className={styles.modalInput}
               name="username"
@@ -111,9 +104,6 @@ function SignUpPage() {
             >
               Зарегистрироваться
             </button>
-           {/*  <Link className={styles.modalBtnSignup} href="/signup">
-              Зарегистрироваться
-            </Link> */}
           </form>
         </div>
       </div>
